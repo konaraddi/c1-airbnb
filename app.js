19f0876bd6dfea438d61b07421934da9c60063be
konaraddio <omkar.konaraddi@gmail.com>
2017-10-23 13:43:26 -0400

    completed chart of avg num of reviews per host per year

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
+// AVERAGE NUMBER OF REVIEWS PER HOST (Y) VS HOST SINCE (X)
 d3.csv("data/csv/listings.csv", function(data) {
+    
+    //avg number of reviews per year
+    var avg_reviews = [];
 
-    // map of years & sum of number reviews per year and number of hosts per year 
-    var xy_map = new Map();
+    var years = [];
+    // 2008 ---> 2017 placed in years
+    for(let i = 0; i < 10; i++){
+        years.push(2008 + i); 
+        avg_reviews.push(null);   
+    }
 
-    for(let i = 0; i < data.length; i++){
-        
-        // get just the year
-        host_since_year = Number(data[i]["host_since"].substring(0,4));
-        if(host_since_year != 0){
-            if(xy_map.get(host_since_year) == undefined){
-                xy_map.set(host_since_year, [data[i]["number_of_reviews"], 1]);
-            }else{
-                // put sum of reviews 
-                // put number of hosts
-                xy_map.set(host_since_year, )
+    for(let j = 0; j < data.length; j++){
+        let year = Number(data[j]["host_since"].substring(0,4));
+        for(let k = 0; k < 10; k++){
+            if(year == (2008 + k)){
+                let num_reviews = Number(data[j]["number_of_reviews"]);
+                if(avg_reviews[k] == null){
+                    avg_reviews[k]=[num_reviews,1];
+                }else{
+                    avg_reviews[k]=[avg_reviews[k][0]+num_reviews, avg_reviews[k][1]+1];
+                }
+                break;
             }
         }
     }
-
-    var x = Array.from(xy_map.keys());
-    var y = [];
-    for(var [key, value] of xy_map){
-        y.push(xy_map.get(key));
-    }
-    console.log(x);
-    console.log(y);
+    console.log(avg_reviews);
     
+    
+    // calculate average number of reviews per host for each year
+    for(let l = 0; l < 10; l++){
+        avg_reviews[l] = Math.round(avg_reviews[l][0] / avg_reviews[l][1]);
+    }
+
+    console.log(avg_reviews);
+
     var ctx = document.getElementById('calendar-chart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
@@ -33,15 +42,16 @@ d3.csv("data/csv/listings.csv", function(data) {
     
         // The data for our dataset
         data: {
-            labels: x,
+            labels: years,
             datasets: [{
-                label: "My First dataset",
-                borderColor: 'rgb(255, 99, 132)',
-                data: y,
+                label: "Average Number of Reviews Per Host",
+                backgroundColor:'rgb(68,108,179)',
+                data: avg_reviews,
             }]
         },
     
         // Configuration options go here
         options: {}
     });
+    
 });
