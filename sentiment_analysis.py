4797d6168dee5f96effdadf9e78850431d4a124f
konaraddio <omkar.konaraddi@gmail.com>
2017-10-29 09:32:16 -0400

    sentiment analysis writes to file

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -10,7 +10,7 @@
 # listing_id in reviews --> id in listings --> neighborhood in listings 
 
 # TODO find ids in reviews first then loop thru again and sum up the confidence level if positive for each id
-# then find top 5 neighborhoods
+# then find most popular neighborhoods
 
 import csv
 import json
@@ -22,48 +22,47 @@ id_confidence = {}
 with open('data/csv/reviews.csv') as reviewsFile:
     csvReader = csv.reader(reviewsFile)
     steps = 0
-    limit = 20
 
     for row in csvReader:
-        # add all unique ids to dictionary
-        if row[0] not in id_confidence:
-            id_confidence[row[0]] = 0
 
-        # add confidence level for each key (in the end, it will be the sum of the confidence levels) 
-        payload={'txt': row[5]}
-        r = requests.post('http://sentiment.vivekn.com/web/text/', data=payload )
-        response = json.loads(r.text)
-        if response["result"] == "Positive":
-            id_confidence[row[0]] += float(response["confidence"])     
+        # sampling some of the reviews
+        if steps % 10000 == 0:
+            # add all unique ids to dictionary
+            if row[0] not in id_confidence:
+                id_confidence[row[0]] = 0
 
-        steps += 1
-        if steps > limit:
-            break  
+            # add confidence level for each key (in the end, it will be the sum of the confidence levels) 
+            payload={'txt': row[5]}
+            r = requests.post('http://sentiment.vivekn.com/web/text/', data=payload )
+            response = json.loads(r.text)
+            if response["result"] == "Positive":
+                id_confidence[row[0]] += float(response["confidence"])     
 
-# find the top 10 most popular neighborhoods
+        steps += 1 
+
+# find the most popular neighborhoods
 import operator
 sorted_id_conf = sorted(id_confidence.items(), key=operator.itemgetter(1))
-top10 = sorted_id_conf[-10:]
-top10 = top10[::-1]
-print("TOP 10", top10)
+top = sorted_id_conf[-len(sorted_id_conf):]
+top = top[::-1]
 
 # find the neighborhood that correponds with each id and write it to a file
 position = 0
-file = open("top10.txt","w")
+neighborhoods_recorded = []
+file = open("top.txt","w")
 with open('data/csv/listings.csv') as listingsFile:
     csvReader = csv.reader(listingsFile)
     
     for row in csvReader:
-        for pair in top10:
-            listing_id = pair[0]
-            # check if id matches up with a top10 id
-            if row[0] == listing_id:
-                # check if the neighborhood is listed
-                if(row[38] != ''):
+        for pair in top:
+            # check if id matches up with a top id
+            if row[0] == pair[0]:
+                # check if the neigborhood is listed for corresponding id and if it already hasn't been recorded
+                if(row[38] != '' and row[38] not in neighborhoods_recorded):
+                    neighborhoods_recorded.append(row[38])
                     position+=1
-                    print(position, row[38])
-                    file.write(position + " " + row[38])
-                    file.write(row[38])
+                    line = str(position) + " " + row[0] + " " + row[38] + "\n"
+                    file.write(line)
                     break
 
 file.close()
