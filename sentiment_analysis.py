2a6a036da841e9aad92292b5f89f02bf157d2e2a
konaraddio <omkar.konaraddi@gmail.com>
2017-10-29 09:12:08 -0400

    working on sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -16,16 +16,54 @@ import csv
 import json
 import requests
 
+# dictionary of ids mapped to the sum of their confidence level
+id_confidence = {}
+
 with open('data/csv/reviews.csv') as reviewsFile:
     csvReader = csv.reader(reviewsFile)
     steps = 0
-    limit = 30
+    limit = 20
 
     for row in csvReader:
+        # add all unique ids to dictionary
+        if row[0] not in id_confidence:
+            id_confidence[row[0]] = 0
+
+        # add confidence level for each key (in the end, it will be the sum of the confidence levels) 
         payload={'txt': row[5]}
         r = requests.post('http://sentiment.vivekn.com/web/text/', data=payload )
-        data = json.loads(r.text)
-        print(data)
-        steps+=1
+        response = json.loads(r.text)
+        if response["result"] == "Positive":
+            id_confidence[row[0]] += float(response["confidence"])     
+
+        steps += 1
         if steps > limit:
-            break
+            break  
+
+# find the top 10 most popular neighborhoods
+import operator
+sorted_id_conf = sorted(id_confidence.items(), key=operator.itemgetter(1))
+top10 = sorted_id_conf[-10:]
+top10 = top10[::-1]
+print("TOP 10", top10)
+
+# find the neighborhood that correponds with each id and write it to a file
+position = 0
+file = open("top10.txt","w")
+with open('data/csv/listings.csv') as listingsFile:
+    csvReader = csv.reader(listingsFile)
+    
+    for row in csvReader:
+        for pair in top10:
+            listing_id = pair[0]
+            # check if id matches up with a top10 id
+            if row[0] == listing_id:
+                # check if the neighborhood is listed
+                if(row[38] != ''):
+                    position+=1
+                    print(position, row[38])
+                    file.write(position + " " + row[38])
+                    file.write(row[38])
+                    break
+
+file.close()
